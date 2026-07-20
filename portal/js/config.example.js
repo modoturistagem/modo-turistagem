71458af010ea597d05bd6ab4a14b640552155444
modoturistagem <modoturistagem@gmail.com>
2026-07-20 14:11:58 -0300

    docs(portal): add Supabase config example

---
+++ portal/js/config.example.js
@@ -0,0 +1,6 @@
+window.MODO_PORTAL_CONFIG = {
+  supabaseUrl: "https://SEU-PROJETO.supabase.co",
+  supabaseAnonKey: "SUA_CHAVE_PUBLICA_ANON",
+  demoMode: false,
+  siteUrl: "https://modoturistagem.github.io/modo-turistagem/portal"
+};
