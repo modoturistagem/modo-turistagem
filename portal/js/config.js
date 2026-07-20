a2ca7b65ac5e98c264169afe84dc10f5ca2a9113
modoturistagem <modoturistagem@gmail.com>
2026-07-20 15:23:22 -0300

    chore(portal): connect Supabase project

--- portal/js/config.js
+++ portal/js/config.js
@@ -1,6 +1,6 @@
 window.MODO_PORTAL_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
-  demoMode: true,
+  supabaseUrl: "https://wztouynvhcxufrmfyoxb.supabase.co",
+  supabaseAnonKey: "sb_publishable_4tsWKac5UHnLon_fozbRoA_82gP_zYj",
+  demoMode: false,
   siteUrl: "https://modoturistagem.github.io/modo-turistagem/portal"
 };
