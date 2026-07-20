562d5132b8ad94d79844581926d5e9d6eedadef3
modoturistagem <modoturistagem@gmail.com>
2026-07-20 16:30:44 -0300

    chore(portal): configure internal username domain

--- portal/js/config.js
+++ portal/js/config.js
@@ -2,5 +2,6 @@ window.MODO_PORTAL_CONFIG = {
   supabaseUrl: "https://wztouynvhcxufrmfyoxb.supabase.co",
   supabaseAnonKey: "sb_publishable_4tsWKac5UHnLon_fozbRoA_82gP_zYj",
   demoMode: false,
+  usernameDomain: "clientes.modoturistagem.local",
   siteUrl: "https://modoturistagem.github.io/modo-turistagem/portal"
 };
